82c7049e4f69c17c45f1654f4da00f43f73b508f
Alexander Wischnewski <alexander.wischnewski@tum.de>
2022-03-19 14:05:36 +0100

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,49 +13,14 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-sys.path.append('./misc/py_binds/tum_mcs')
 
 # -- Project information -----------------------------------------------------
 
 project = 'Vehicle Dynamics Control'
-copyright = '2020, Alexander Wischnewski'
+copyright = '2022, Alexander Wischnewski'
 author = 'Alexander Wischnewski'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_automodapi.automodapi'
-]
-
-autoclass_content = 'both'
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'classic'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# EOF
